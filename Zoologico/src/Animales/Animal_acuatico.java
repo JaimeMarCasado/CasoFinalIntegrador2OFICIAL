24e73d39519976adb8b116746d4a41e8797b6137
Jaime Martin <jaime.martincasado@usp.ceu.es>
2024-03-01 10:56:02 +0100

    cambio

--- Zoologico/src/Animales/Animal_acuatico.java
+++ Zoologico/src/Animales/Animal_acuatico.java
@@ -1,13 +1,15 @@
-public package Zoologico.src.Animales;
+package Animales;
 
-public class Animal_acuatico extends Animal{
+import Animales.Animal;
+
+public class Animal_acuatico extends Animal {
     boolean aletas;
     boolean branquias;
     boolean escamas;
     String tipo_agua;
 
     public Animal_acuatico(String nombre, String especie, Boolean salud, Boolean comportamiento, String alimentacion, int ID, boolean aletas, boolean branquias, boolean escamas, String tipo_agua ){
-        super(nombre, especie, salud, comportamineto , alimentacion, ID);
+        super(nombre, especie, salud, comportamiento , alimentacion, ID);
         this.aletas = aletas;
         this.branquias = branquias;
         this.escamas = escamas;
